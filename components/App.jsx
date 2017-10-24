d1a977366c8efb5133c308d7926bf8439ee06063
Mihai <mihaiveronica@ymail.com>
2017-10-24 09:59:15 +0300

    add initial setup with webpack and React

---
+++ components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../app.scss';
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return (
+            <div className="mainContainer">
+                Hello World
+             </div>
+        )
+    };
+}
+export default App;
